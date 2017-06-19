dad3db3966eba32424392ff2e0e2168eaa914f2c
Daikinity <dai.kon1006@gmail.com>
2017-06-20 00:17:35 +0900

    Add files via upload

---
+++ workshop/mi_card/assets/js/scripts.js
@@ -0,0 +1,94 @@
+// JS 
+// (function(){
+// 	function click(){
+// 		var element = document.getElementById('slide');
+// 		element.addEventListener('click',function(){
+// 			element.slideDown('slow');
+// 		});
+// 	};
+// });
+
+$('#slide1').click(function(){
+  if ($('.target1').css('display') == 'none' && $('.fa-angle-up').css('display') == 'none') {
+    $('.target1').slideDown('slow');
+    $('.hideicon1').show();
+    $('.icon1').hide();
+  }else{
+    $('.target1').slideUp('slow');
+    $('.hideicon1').hide();
+    $('.icon1').show();
+  }
+});
+
+$('#slide2').click(function(){
+  if ($('.target2').css('display') == 'none' && $('.fa-angle-up').css('display') == 'none') {
+    $('.target2').slideDown('slow');
+    $('.hideicon2').show();
+    $('.icon2').hide();
+  }else{
+    $('.target2').slideUp('slow');
+    $('.hideicon2').hide();
+    $('.icon2').show();
+  }
+});
+
+$('#slide3').click(function(){
+  if ($('.target3').css('display') == 'none' && $('.fa-angle-up').css('display') == 'none') {
+    $('.target3').slideDown('slow');
+    $('.hideicon3').show();
+    $('.icon3').hide();
+  }else{
+    $('.target3').slideUp('slow');
+    $('.hideicon3').hide();
+    $('.icon3').show();
+  }
+});
+
+$('#slide4').click(function(){
+  if ($('.target4').css('display') == 'none' && $('.fa-angle-up').css('display') == 'none') {
+    $('.target4').slideDown('slow');
+    $('.hideicon4').show();
+    $('.icon4').hide();
+  }else{
+    $('.target4').slideUp('slow');
+    $('.hideicon4').hide();
+    $('.icon4').show();
+  }
+});
+
+$('#slide5').click(function(){
+  if ($('.target5').css('display') == 'none' && $('.fa-angle-up').css('display') == 'none') {
+    $('.target5').slideDown('slow');
+    $('.hideicon5').show();
+    $('.icon5').hide();
+  }else{
+    $('.target5').slideUp('slow');
+    $('.hideicon5').hide();
+    $('.icon5').show();
+  }
+});
+
+$('#slide6').click(function(){
+  if ($('.target6').css('display') == 'none' && $('.fa-angle-up').css('display') == 'none') {
+    $('.target6').slideDown('slow');
+    $('.hideicon6').show();
+    $('.icon6').hide();
+  }else{
+    $('.target6').slideUp('slow');
+    $('.hideicon6').hide();
+    $('.icon6').show();
+  }
+});
+
+
+$('#slide7').click(function(){
+  if ($('.target7').css('display') == 'none' && $('.fa-angle-up').css('display') == 'none') {
+    $('.target7').slideDown('slow');
+    $('.hideicon7').show();
+    $('.icon7').hide();
+  }else{
+    $('.target7').slideUp('slow');
+    $('.hideicon7').hide();
+    $('.icon7').show();
+  }
+});
